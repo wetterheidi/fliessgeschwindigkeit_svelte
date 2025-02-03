ff96d0a28d4e9d79f96d92cd239116326b0734f7
“wetterheidi“ <43071660+wetterheidi@users.noreply.github.com>
2025-02-03 17:26:40 +0100

    Setup GitHub Pages deployment

---
+++ svelte.config.js
@@ -0,0 +1,9 @@
+const config = {
+    kit: {
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/fliessgeschwindigkeit_svelte' : ''
+        }
+    }
+};
+
+export default config;
