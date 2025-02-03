71452e4a27a3927ac9d621153e64e5dbef2679c6
wetterheidi <43071660+wetterheidi@users.noreply.github.com>
2025-02-03 19:48:04 +0000

    Deploying to gh-pages from @ wetterheidi/fliessgeschwindigkeit_svelte@efb89176483926fad495ace51c1d676f87d01f08 🚀

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/fliessgeschwindigkeit_svelte/sw.js', { scope: '/fliessgeschwindigkeit_svelte/' })})}
