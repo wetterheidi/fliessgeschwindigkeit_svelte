401be2a58929e5e8ee10d68ef7f5944c8d127dc6
“wetterheidi“ <43071660+wetterheidi@users.noreply.github.com>
2025-02-03 18:01:57 +0100

    Fix GitHub Pages deployment workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
+import adapter from '@sveltejs/adapter-static';
+
 const config = {
     kit: {
+        adapter: adapter(),
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/fliessgeschwindigkeit_svelte' : ''
         }
