5cce12d57bb714c6774e13fe4a29965ea8e389be
“wetterheidi“ <43071660+wetterheidi@users.noreply.github.com>
2025-02-03 19:09:16 +0100

    Try No 13

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default defineConfig({
   plugins: [svelte()],
-  base: '/fliessgeschwindigkeit/',
+  base: '/fliessgeschwindigkeit_svelte/',
   build: {
     outDir: 'dist',
     sourcemap: true
